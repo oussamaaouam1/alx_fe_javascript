b0de147e3c3b8d6422fd93b7920fafdfc524a629
oussamaaouam1 <oussama.aouam@gmail.com>
2024-07-27 00:18:16 +0100

    commit the 1st part

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -8,8 +8,18 @@ document.addEventListener('DOMContentLoaded',()=>{
   ];
   //function to display a quote randomly ======
   const showRandomQuote = ()=>{
-  const quoteDisplay = document.getElementById('quoteDisplay')
+  const quoteDisplay = document.getElementById('quoteDisplay');
   const randomSelector = Math.floor(Math.random()*quotes.length);
   const randomQuote = quotes[randomSelector];
+  quoteDisplay.innerHTML='';
+    //creat a parag to display the quote=======
+  const quoteTextElement = document.createElement('p');
+  quoteTextElement.textContent = `${randomQuote.text}`;
+  const quoteCategoryElement = document.createElement('p');
+  quoteCategoryElement.textContent = `-${randomQuote.category}`
+  //append child to display the quotes============
+  quoteDisplay.appendChild(quoteTextElement);
+  quoteDisplay.appendChild(quoteCategoryElement);
   }
+  document.getElementById('newQuote').addEventListener('click', showRandomQuote);
 });
