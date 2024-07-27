dc92451ba5f2412cc8aaf531512c0a74cf161668
oussamaaouam1 <oussama.aouam@gmail.com>
2024-07-27 11:18:48 +0100

    commit the part 1 of task 2

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,11 +1,18 @@
 document.addEventListener('DOMContentLoaded',()=>{
   //quotes creation ===========================
 
-  let quotes = [
+  let quotes =  [
     { text: "Life is what happens when you're busy making other plans.", category: "Life" },
     { text: "The purpose of our lives is to be happy.", category: "Happiness" },
     { text: "Get busy living or get busy dying.", category: "Motivation" }
-  ];
+  ] || JSON.parse(localStorage.getItem('quotes'));
+
+  // Function to save quotes to local storage
+  const saveQuotes = ()=>{
+    localStorage.setItem('quotes',JSON.stringify(quotes));
+    console.log(quotes);
+  }
+
   //function to display a quote randomly ======
   const showRandomQuote = ()=>{
   const quoteDisplay = document.getElementById('quoteDisplay');
@@ -31,16 +38,16 @@ document.addEventListener('DOMContentLoaded',()=>{
       quotes.push({text:newQuoteText , category:newQuoteCategory});
       if (newQuoteCategory!==''&&newQuoteText!=='') {
         alert('quotes add successfully');
-      }else{
-        alert('please enter a valid text and category')
       }
       document.getElementById('newQuoteText').value = '';
       document.getElementById('newQuoteCategory').value = '';
-  
-
-      
+    }
+    else if (newQuoteCategory =="" || newQuoteText =="") {
+      alert('please enter a valid text and category')
     }
   }
+
   document.getElementById('newQuote').addEventListener('click', showRandomQuote);
   document.getElementById('addQuoteButton').addEventListener('click',createAddQuoteForm);
+  document.getElementById('addQuoteButton').addEventListener('click',saveQuotes);
 });
